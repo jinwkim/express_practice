f0a7e64a238cb125bcb16fbefd4c6049b38066bb
Jin Kim <jinkim1195@gmail.com>
2020-12-28 11:26:12 -0800

    I learned that for...of... is the right one for iterating through objects in array. I learned that I need to accept urlencoded/json to be able to post/patch data.

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 
-app.set("view engine","ejs");
+// In order to accept data...
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.json());
+
+// Since forms can only do GET/POST, we need this to do PATCH
+app.use(methodOverride('_method'));
+
 app.set("views", path.join(__dirname, '/views'));
+app.set("view engine","ejs");
+
+let dataset = [
+    { 
+        username: "jkim", 
+        id: 0, 
+        comment: "this is a bad comment"
+    },
+    { 
+        username: "foo", 
+        id: 1, 
+        comment: "this is a good comment"
+    },
+    { 
+        username: "bar", 
+        id: 2, 
+        comment: "this is just one comment"
+    }
+];
 
 app.get("/", (req,res) => {
     // console.log("welcome to /");
-    res.render("home");
-
+    res.render("home"); 
 });
 
 app.get("/comments", (req,res)=> {
-    res.send("This is the comments page");
+    // console.log(dataset);
+    res.render("comments", { dataset } );
 });
+
+app.get("/comments/:id", (req,res)=> {
+    const { id } = req.params;
+    const currobj = dataset[id];
+    res.render("editcomments", { currobj });
+});
+
+// update a comment using PATCH
+app.patch("/comments/:id", (req,res) => {
+    const { id } = req.params;
+    console.log(id);
+
+    const newtext = req.body.comment;
+    console.log(newtext);
+
+    dataset[id].comment = newtext;
+
+    res.redirect("/");
+});
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
